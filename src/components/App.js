bff10ff30410d3f49a535bf4cb9d43308798e52b
softo-mayur <mayurbhardwaj99@gmail.com>
2026-07-05 18:45:37 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
+  const relatives = [
+    "Father",
+    "Mother",
+    "Uncle",
+    "Aunt",
+    "Cousin"
+  ];
+
   return (
     <div id="main">
-               {/* Do not remove the main div */}
-               <PageLayout   header={<h1>Header</h1>}footer={<footer>Footer</footer>}>
-        <main>Main Content</main>
-        </PageLayout>
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      <ol key="relativeList">
+        {relatives.map((relative, index) => (
+          <li key={`relativeListItem${index + 1}`}>
+            {relative}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default App;
